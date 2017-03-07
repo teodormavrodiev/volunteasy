8a95b38ca36d03cff98a79121e87052a1dc71b11
Teodor Mavrodiev <teodor.mavrodiev@gmail.com>
2017-03-07 15:39:58 +0100

    fix bugs with dependent destroy

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :organizer, class_name: "User", foreign_key: "organizer_id"
 
-  has_many :registrations
+  has_many :registrations, dependent: :destroy
   has_many :participants, through: :registrations
 
   # Photo implementation
