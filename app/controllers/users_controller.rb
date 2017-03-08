44e6b931872e02343e88420c842c4b943c95432f
Teodor Mavrodiev <teodor.mavrodiev@gmail.com>
2017-03-08 19:05:27 +0100

    add cards to users home page also

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,29 @@ class UsersController < ApplicationController
   def show
     # Display the public profile of a specific user
     @user = User.find(params[:id])
+
+    @past_events_attended = past_events_attendee(@user)
+    @current_events_attended = current_events_attendee(@user)
+    @past_events_managed = past_events_manager(@user)
+    @current_events_managed = current_events_manager(@user)
+  end
+
+  private
+
+  def past_events_manager(user)
+    user.events.where("end_time < ?", DateTime.now)
+  end
+
+  def current_events_manager(user)
+    user.events.where("end_time > ?", DateTime.now)
+  end
+
+  def past_events_attendee(user)
+    user.events_as_participant.where("end_time < ?", DateTime.now)
+  end
+
+  def current_events_attendee(user)
+    user.events_as_participant.where("end_time > ?", DateTime.now)
   end
 
 end
