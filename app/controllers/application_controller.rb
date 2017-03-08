b3670ec91704bce3288dcad9e972432029177588
Teodor Mavrodiev <teodor.mavrodiev@gmail.com>
2017-03-08 15:01:45 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   include Pundit
 
   # Pundit: white-list approach.
-  after_action :verify_authorized, except: :search, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :search, unless: :skip_pundit?
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
