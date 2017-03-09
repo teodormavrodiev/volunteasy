aa80dbb7deb42b2749b91b3a06166b5bc918aa2d
James Becker <jamsbec@gmail.com>
2017-03-09 16:30:27 +0100

    fix trilio

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,5 @@
+require 'twilio-ruby'
+
 class Registration < ApplicationRecord
   belongs_to :participant, class_name: 'User', foreign_key: 'participant_id'
   belongs_to :event
@@ -5,27 +7,28 @@ class Registration < ApplicationRecord
   validates :participant_id, presence: true
   validates :event_id, presence: true
 
-  after_create :reminder, unless: Proc.new { !self.notifiable }
+  after_create :reminder
 
   @@REMINDER_TIME = 120.minutes # minutes before appointment
 
   # Notify our appointment attendee 2 hours before the appointment time
   def reminder
+    puts "Creating Message"
     @twilio_number = ENV['TWILIO_NUMBER']
     @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
     time_str = ((self.event.start_time).localtime).strftime("%I:%M%p on %b. %d, %Y")
     reminder = "Hi #{self.participant.first_name}! This is a reminder to let you know that #{self.event.name} starts at #{time_str}. We hope you have a great time! - Team Volunteasy"
     message = @client.account.messages.create(
       from: @twilio_number,
-      to: self.participant.phone,
+      to: '652720500',
       body: reminder,
     )
     puts message.to
   end
 
   def when_to_run
-    time - @@REMINDER_TIME
+    self.event.start_time - @@REMINDER_TIME
   end
 
-  handle_asynchronously :reminder, run_at: Proc.new { |i| i.when_to_run }
+  # handle_asynchronously :reminder, run_at: Proc.new { |i| i.when_to_run }
 end
