712586831242ccc9d43dc363a10ad50228910c69
juliette engel <juliette.engel@skema.edu>
2017-03-16 18:38:39 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,13 +19,28 @@ ruben = User.new({
   phone: "0909090909",
   photo_url: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAhQAAAAJDFlZjZmZmYwLWQ5ZTgtNDc1ZS1iNzM1LWJmZmQ3Mjc5OWE2Yw.jpg"
   })
+
+
+johnny = User.new({
+  email: "johnny@gmail.com",
+  password: "rosebud",
+  password_confirmation: "rosebud",
+  first_name: "Johnny",
+  last_name: "Deep",
+  organization: "DailyHelp",
+  phone: "0909090909",
+  photo_url: "http://xewt12.com/wp-content/uploads/2016/02/johnny-depp-dior-vogue-3jun15-pr_b.jpg"
+  })
+
+
+
 jackie = User.new({
   email: "jackie-kennedy@kennedy.com",
   password: "rosebud",
   password_confirmation: "rosebud",
   first_name: "Jackie",
   last_name: "Kenny",
-  organization: "Language Integration for Syrian Refugee Children",
+  organization: "nationalbreastcancer.org, Language Integration for Syrian Refugee Children",
   phone: "0908076532",
   photo_url: "https://www.biography.com/.image/c_fit,cs_srgb,dpr_1.0,q_80,w_620/MTE5NTU2MzE2MzM0NTU2Njgz/jacqueline-kennedy-onassis-9428644-2-raw.jpg"
   })
@@ -70,10 +85,25 @@ The program I facilitate is called the 'Sandwich Run' which involves volunteers 
   })
 
 
+james = User.new({
+  email: "james@gmail.com",
+  password: "james@gmail.com",
+  password_confirmation: "james@gmail.com",
+  organization: "El Refugio Barcelona",
+  first_name: "James",
+  last_name: "B",
+  phone: "09080787632",
+  bio: "I like to help others but it wasn't until i found Volunteasy that my life changed forever. Now i can help people whenever I want :) Most important : I love puppies.",
+  photo_url: "https://avatars3.githubusercontent.com/u/5620278?v=3&s=400"
+  })
+
+
 alex.save
+johnny.save
 ruben.save
 jackie.save
 jane.save
+james.save
 jeanjacques.save
 puts ruben.attributes
 puts jeanjacques.attributes
@@ -105,10 +135,32 @@ julia = User.new({
   last_name: "Roberts",
   photo_url: "http://res.cloudinary.com/demo/image/fetch/https://s-media-cache-ak0.pinimg.com/originals/9f/71/12/9f71124b93343d50f68548af271a54db.jpg"
   })
+mariah = User.new({
+  email: "mariah@gmail.com",
+  password: "rosebud",
+  password_confirmation: "rosebud",
+  first_name: "Mariah",
+  last_name: "Triangle",
+  photo_url: "http://www.billboard.com/files/styles/article_main_image/public/media/mariah-carey-july-2014-billboard-650.jpg"
+  })
+
+marina =  User.new({
+  email: "marina@gmail.com",
+  password: "rosebud",
+  password_confirmation: "rosebud",
+  first_name: "Marina",
+  last_name: "Elpaso",
+  bio: 'As a volunteer, I’ve learned that no matter how troubled our past, how despairing our present we all have one thing in common – our humanity. We are all struggling to survive and find our place in the world. I have learned how to be a support for others without getting bogged down by the pain of others.',
+  photo_url: "http://ramblingmandie.com/wp-content/uploads/2014/09/girl-volunteering.jpg"
+  })
 
 
 
+
+
+mariah.save
 joe.save
+marina.save
 julia.save
 jimmy.save
 puts joe.attributes
@@ -116,7 +168,7 @@ puts julia.attributes
 puts jimmy.attributes
 puppy_feeding = Event.new({
   name: "Puppy feeding",
-  organizer_id: ruben.id,
+  organizer_id: james.id,
   start_time: DateTime.strptime("19:00 10/03/2017", "%H:%M %d/%m/%Y"),
   end_time: DateTime.strptime("21:00 10/03/2017", "%H:%M %d/%m/%Y"),
   address: "Refugi, Carrer Nou de la Rambla, 175, 08004 Barcelona",
@@ -130,10 +182,10 @@ puppy_feeding = Event.new({
 
 
 puppy_feeding2 = Event.new({
-  name: "Puppy feeding",
+  name: "Help us take care of an orphaned litter of puppies",
   organizer_id: ruben.id,
-  start_time: DateTime.strptime("14:00 01/04/2017", "%H:%M %d/%m/%Y"),
-  end_time: DateTime.strptime("18:00 01/04/2017", "%H:%M %d/%m/%Y"),
+  start_time: DateTime.strptime("14:00 13/03/2017", "%H:%M %d/%m/%Y"),
+  end_time: DateTime.strptime("18:00 13/03/2017", "%H:%M %d/%m/%Y"),
   address: "Refugi, Carrer Nou de la Rambla, 175, 08004 Barcelona",
   capacity: 8,
   description: "Help us feeding puppies at Barcelona shelter!",
@@ -146,8 +198,8 @@ puppy_feeding2 = Event.new({
 teaching_refugees_spanish = Event.new({
   name: "Teach refugees spanish",
   organizer_id: jackie.id,
-  start_time: DateTime.strptime("19:00 01/03/2017", "%H:%M %d/%m/%Y"),
-  end_time: DateTime.strptime("21:00 01/03/2017", "%H:%M %d/%m/%Y"),
+  start_time: DateTime.strptime("19:00 19/03/2017", "%H:%M %d/%m/%Y"),
+  end_time: DateTime.strptime("21:00 19/03/2017", "%H:%M %d/%m/%Y"),
   address: "Centro del Estudio, Carrer de la Princesa, 08003 Barcelona",
   capacity: 8,
   description: "No specific skills required, just a desire to help and a friendly smile",
@@ -174,8 +226,8 @@ teaching_refugees_spanish = Event.new({
 teaching_refugees_english = Event.new({
   name: "Teach refugees english",
   organizer_id: jackie.id,
-  start_time: DateTime.strptime("19:00 19/03/2017", "%H:%M %d/%m/%Y"),
-  end_time: DateTime.strptime("21:00 19/03/2017", "%H:%M %d/%m/%Y"),
+  start_time: DateTime.strptime("19:00 22/03/2017", "%H:%M %d/%m/%Y"),
+  end_time: DateTime.strptime("21:00 22/03/2017", "%H:%M %d/%m/%Y"),
   address: "Centro del Estudio, Carrer de la Princesa, 08003 Barcelona",
   capacity: 8,
   description: "No specific skills required, just a desire to help and a friendly smile",
@@ -185,7 +237,7 @@ teaching_refugees_english = Event.new({
   ]
   })
 beach_cleaning = Event.new({
-  name: "let's clean the beach!",
+  name: "Let's clean the beach!",
   organizer_id: jeanjacques.id,
   start_time: DateTime.strptime("19:00 20/03/2017", "%H:%M %d/%m/%Y"),
   end_time: DateTime.strptime("21:00 20/03/2017", "%H:%M %d/%m/%Y"),
@@ -202,7 +254,7 @@ nursing_home = Event.new({
   organizer_id: jeanjacques.id,
   start_time: DateTime.strptime("19:00 16/03/2017", "%H:%M %d/%m/%Y"),
   end_time: DateTime.strptime("21:00 16/03/2017", "%H:%M %d/%m/%Y"),
-  address: "Paradise Nursing Home, calle Sardenya, 08765 Manresa",
+  address: "Paradise Nursing Home, calle Sardenya, 08765 Barcelona",
   capacity: 5,
   description: "Come for a chat and share a meal with our residents",
   tags: ["Seniors", "People with disabilities"],
@@ -215,7 +267,7 @@ plant_trees = Event.new({
   organizer_id: jeanjacques.id,
   start_time: DateTime.strptime("19:00 08/03/2017", "%H:%M %d/%m/%Y"),
   end_time: DateTime.strptime("21:00 08/03/2017", "%H:%M %d/%m/%Y"),
-  address: "Selva de Montseny, 08766 Monseny",
+  address: "Selva de Montseny, 08766 Barcelona",
   capacity: 5,
   description: "We'll plant trees and discuss about local initiatives",
   tags: ["Environment"],
@@ -228,7 +280,7 @@ sewing = Event.new({
   organizer_id: jackie.id,
   start_time: DateTime.strptime("19:00 10/03/2017", "%H:%M %d/%m/%Y"),
   end_time: DateTime.strptime("21:00 10/03/2017", "%H:%M %d/%m/%Y"),
-  address: "3 calle de la Flor, 08766 Madrid",
+  address: "3 calle de la Flor, 08766 Barcelona",
   capacity: 14,
   description: "We partner with individuals ages 60+ to foster community connections and improve quality of life.",
   tags: ["Community"],
@@ -241,7 +293,7 @@ business = Event.new({
   organizer_id: jackie.id,
   start_time: DateTime.strptime("19:00 15/03/2017", "%H:%M %d/%m/%Y"),
   end_time: DateTime.strptime("21:00 15/03/2017", "%H:%M %d/%m/%Y"),
-  address: "3 calle de Catalunya, 08766 Madrid",
+  address: "3 calle de Catalunya, 08766 Barcelona",
   capacity: 14,
   description: "Volunteer as a mentor on MicroMentor.org and make a meaningful connection with a business owner. ",
   tags: ["Community"],
@@ -252,14 +304,13 @@ business = Event.new({
 
 
 bike = Event.new({
-  name: "Volunteers needed at the Bike MS: BCN Ride - Hepatitis C Trust",
+  name: "Volunteers needed at the BCN Ride Event!",
   organizer_id: jackie.id,
   start_time: DateTime.strptime("15:00 18/03/2017", "%H:%M %d/%m/%Y"),
   end_time: DateTime.strptime("20:00 18/03/2017", "%H:%M %d/%m/%Y"),
   address: "Rambla del Raval, Barcelona",
-  capacity: 14,
-  description: "Volunteer at this exciting one day bike ride and raise money for for The Hepatitis C Trust
-Volunteers are needed to help with things like set-up, check-in, food service, cheering, tear down and rest stops.",
+  capacity: 15,
+  description: "Raise money for breast cancer research! Volunteer at this exciting one day bike ride and raise money for for The Hepatitis C Trust! Volunteers are needed to help with things like set-up, check-in, food service, cheering, tear down and rest stops.",
   tags: ["Sports", "Health"],
   photo_urls: [
     "http://res.cloudinary.com/demo/image/fetch/https://farm6.staticflickr.com/5545/14306218244_892c83fe49.jpg"
@@ -329,7 +380,7 @@ fresh = Event.new({
 
 hospice3 = Event.new({
   name: "Animal Shelter Volunteers needed!",
-  organizer_id: jeanjacques.id,
+  organizer_id: james.id,
   start_time: DateTime.strptime("10:00 19/03/2017", "%H:%M %d/%m/%Y"),
   end_time: DateTime.strptime("14:00 19/03/2017", "%H:%M %d/%m/%Y"),
   address: "3 carrer de la Unio, Raval, Barcelona",
@@ -351,7 +402,7 @@ homeless1 = Event.new({
   capacity: 15,
   description: "Hello All:
 Please join me on this sandwich run.
-We will be delivering nutritious bagged lunches to the homeless by walking well travelled routes in downtown Toronto where street people live. You will be delivering the lunches with an experienced team leader, who will guide you every step of the way.",
+We will be delivering nutritious bagged lunches to the homeless by walking well travelled routes in downtown Raval where street people live. You will be delivering the lunches with an experienced team leader, who will guide you every step of the way.",
   tags: ["Homeless", "Community"],
   photo_urls: [
     "http://res.cloudinary.com/demo/image/fetch/https://media.glassdoor.com/l/7b/68/71/41/sandwich-making-for-the-homeless.jpg"
@@ -361,14 +412,14 @@ We will be delivering nutritious bagged lunches to the homeless by walking well 
 
 homeless0 = Event.new({
   name: "Sandwiches, Socks or Bottled Water for the Homeless",
-  organizer_id: jane.id,
+  organizer_id: james.id,
   start_time: DateTime.strptime("20:00 12/03/2017", "%H:%M %d/%m/%Y"),
   end_time: DateTime.strptime("22:00 12/03/2017", "%H:%M %d/%m/%Y"),
   address: "Parc de la Ciutadella, Barcelona",
   capacity: 15,
   description: "Hello All:
 Please join me on this sandwich run.
-We will be delivering nutritious bagged lunches to the homeless by walking well travelled routes in downtown Toronto where street people live. You will be delivering the lunches with an experienced team leader, who will guide you every step of the way.",
+We will be delivering nutritious bagged lunches to the homeless by walking well travelled routes in downtown Raval where street people live. You will be delivering the lunches with an experienced team leader, who will guide you every step of the way.",
   tags: ["Homeless", "Community"],
   photo_urls: [
     "http://res.cloudinary.com/demo/image/fetch/https://media.glassdoor.com/l/7b/68/71/41/sandwich-making-for-the-homeless.jpg"
@@ -427,7 +478,7 @@ old1.save
 
 registration_one = Registration.new({
   participant_id: joe.id,
-  event_id: nursing_home.id
+  event_id: bike.id
   })
 registration_two = Registration.new({
   participant_id: joe.id,
@@ -461,6 +512,100 @@ registration_nine = Registration.new({
   participant_id: julia.id,
   event_id: plant_trees.id
   })
+registration_nine = Registration.new({
+  participant_id: julia.id,
+  event_id: plant_trees.id
+  })
+registration_ten = Registration.new({
+  participant_id: johnny.id,
+  event_id: homeless1.id
+  })
+
+registration_twelve = Registration.new({
+  participant_id: johnny.id,
+  event_id: bike.id
+  })
+
+registration_eleven = Registration.new({
+  participant_id: johnny.id,
+  event_id: teaching_refugees_english.id
+  })
+
+registration_thirt = Registration.new({
+  participant_id: julia.id,
+  event_id: bike.id
+  })
+
+registration_forte = Registration.new({
+  participant_id: jimmy.id,
+  event_id: bike.id
+  })
+
+
+registration_15 = Registration.new({
+  participant_id: jimmy.id,
+  event_id: homeless0.id
+  })
+
+registration_16 = Registration.new({
+  participant_id: johnny.id,
+  event_id: homeless0.id
+  })
+
+registration_17 = Registration.new({
+  participant_id: julia.id,
+  event_id: homeless0.id
+  })
+
+registration_18 = Registration.new({
+  participant_id: joe.id,
+  event_id: homeless0.id
+  })
+
+registration_19 = Registration.new({
+  participant_id: marina.id,
+  event_id: homeless0.id
+  })
+
+registration_20 = Registration.new({
+  participant_id: mariah.id,
+  event_id: homeless0.id
+  })
+
+registration_22 = Registration.new({
+  participant_id: marina.id,
+  status: :complete,
+  event_id: business.id,
+  })
+
+registration_21 = Registration.new({
+  participant_id: marina.id,
+  status: :complete,
+  event_id: puppy_feeding.id,
+  })
+
+registration_23 = Registration.new({
+  participant_id: marina.id,
+  status: :complete,
+  event_id: nursing_home.id,
+  })
+
+
+registration_23.save
+registration_22.save
+registration_21.save
+registration_20.save
+registration_19.save
+registration_18.save
+registration_17.save
+registration_16.save
+registration_15.save
+registration_forte.save
+registration_ten.save
+registration_nine.save
+registration_eleven.save
+registration_twelve.save
+registration_thirt.save
 registration_one.save
 registration_two.save
 registration_three.save
@@ -479,10 +624,11 @@ reg11 = Registration.new({
   event_id: old1.id,
   }).save
 reg12 = Registration.new({
-  participant_id: julia.id,
-  event_id: old1.id,
+  participant_id: mariah.id,
+  event_id: bike.id,
   }).save
 
 
 
 puts "///////////////////"
+puts bike.attributes
